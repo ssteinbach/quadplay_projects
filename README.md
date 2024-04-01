cf947caeb18aa7ca26bafe78a718e0c34d8d9dd4
ssteinbach <ssteinbach@users.noreply.github.com>
2024-03-31 22:38:31 -0700

    ok theoretically generate the markdown

--- README.md
+++ README.md
@@ -6,12 +6,12 @@ This repo contains a number of prototypes in various stages of completion.
 
 ## Contents
 
-### Gridpusher
 
+### gridpusher
 |label|[![gridpusher.gridpusher.DEPLOY](gridpusher.gridpusher.DEPLOY/label128.png)](https://morgan3d.github.io/quadplay/console/quadplay.html?game=https://ssteinbach.github.io/quadplay_projects/gridpusher.gridpusher.DEPLOY/gridpusher.game.json)|
-|-|-----------------|
+|-|-----|
 |branch|gridpusher.DEPLOY|
-|description|Iterating on the deployment system for gridpusher|
-|Deployed on:|3/30/2024|
-|link|[play gridpusher.gridpusher.DEPLOY](https://morgan3d.github.io/quadplay/console/quadplay.html?game=https://ssteinbach.github.io/quadplay_projects/gridpusher.gridpusher.DEPLOY/gridpusher.game.json)|
+|description|No description on branch.|
+|Deployed on|03/31/2024|
+|link|[play gridpusher](https://morgan3d.github.io/quadplay/console/quadplay.html?game=https://ssteinbach.github.io/quadplay_projects/gridpusher.gridpusher.DEPLOY/gridpusher.game.json)|
 
--- update_readme.py
+++ update_readme.py
@@ -25,6 +25,10 @@ CONTENT_TEMPLATE = """
 |[play {dirname}]({game_url})|
 """
 
+GAME_HEADER_TEMPLATE = """
+### {gamename}
+"""
+
 
 def main():
     all_stuff = os.listdir(".")
@@ -36,26 +40,97 @@ def main():
     m = re.match(r".*:(?P<username>.*)/(?P<reponame>.*)\.git", git_remote)
     username = m.group("username")
     reponame = m.group("reponame")
-    text = ""
+
+    DATA = {
+        "username": username,
+        "reponame": reponame,
+        "games": {},
+    }
+
     for d in subdirs:
-        game_json = next(t for t in os.listdir(d) if t.endswith(".game.json"))
-        game_url = URL_TEMPLATE.format(
+        game_data = {}
+
+        # should only be one game.json
+        game_data["jsonpath"] = next(
+            t for t in os.listdir(d) if t.endswith(".game.json")
+        )
+        game_data["url"] = URL_TEMPLATE.format(
             gh_user=username,
             reponame=reponame,
             dirname=d,
-            gamejson=game_json,
+            gamejson=game_data["jsonpath"],
         )
-        text += CONTENT_TEMPLATE.format(
-            dirname=d,
-            game_url=game_url,
-        )
+
+        m = re.match(r"(?P<game_name>.*?)\.(?P<branch_name>.*)", d)
+        game_data["name"] = m.group("game_name")
+        game_data["branch"] = m.group("branch_name")
+        game_data["dirname"] = d
+
+        game_data["commit-date-unix"] = subprocess.check_output(
+            [
+                "git",
+                "--work-tree", d,
+                "log",
+                "-n", "1",
+                "--pretty=format:%ct"
+            ]
+        ).decode("utf-8")
+        game_data["commit-date-readable"] = subprocess.check_output(
+            [
+                "git",
+                "--work-tree", d,
+                "log",
+                "-n", "1",
+                "--date=format-local:%m/%d/%Y",
+                "--pretty=format:%cd",
+            ]
+        ).decode("utf-8")
+        # @TODO: this needs to be left in the source repo, maybe a small json
+        #        blob? or into a file
+        try:
+            with open(os.path.join(d, "description.md")) as fi:
+                game_data["description"] = fi.read()
+        except FileNotFoundError:
+            game_data["description"] = "No description on branch."
+        DATA["games"].setdefault(game_data["name"], []).append(game_data)
+
+    content_txt = ""
+    for gamename, builds in DATA["games"].items():
+        builds = sorted(builds, key=lambda b: b["commit-date-unix"])
+        label = "|label|"
+        separator = "|-|"
+        branch = "|branch|"
+        description = "|description|"
+        time = "|Deployed on|"
+        link = "|link|"
+        content_txt += GAME_HEADER_TEMPLATE.format(gamename=gamename)
+
+        for build in builds:
+
+            dirname = game_data["dirname"]
+
+            label += (
+                f'[![{dirname}]({dirname}/label128.png)]({game_data["url"]})|'
+            )
+            separator += "-----|"
+            branch += f'{game_data["branch"]}|'
+            description += f'{game_data["description"]}|'
+            time += f'{game_data["commit-date-readable"]}|'
+            link += f'[play {game_data["name"]}]({game_data["url"]})|'
+
+        content_txt += label + "\n"
+        content_txt += separator + "\n"
+        content_txt += branch + "\n"
+        content_txt += description + "\n"
+        content_txt += time + "\n"
+        content_txt += link + "\n"
 
     with open("README.template.md", 'r') as fi:
         header = fi.read()
 
     with open("README.md", 'w') as fo:
         fo.write(
-            header.format(contents=text)
+            header.format(contents=content_txt)
         )
 
     print("updated readme.")
